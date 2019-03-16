cc336b13b86a17ecdd0412f11a6161e350e8fba4
Antonio Jesus Heredia <a.heredia.castillo@gmail.com>
2019-03-16 12:27:31 +0100

    Casi acabado el apartado del gradiente

--- Practica1/practica1.py
+++ Practica1/practica1.py
@@ -6,7 +6,6 @@ Created on Wed Mar 13 18:16:57 2019
 @author: Antonio Jesús Heredia Castillo
 Practica 1
 """
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
@@ -33,12 +32,15 @@ def gradiente_descendiente(_lr, _punto, _funcion, _iteraciones):
     _dy = sp.diff(funcion, y)
     _lam_dx = sp.lambdify((x,y),_dx) #Lambdifyco las derivadas parciales para calcular mas rapido el valor
     _lam_dy = sp.lambdify((x,y),_dy)
+    _lam_f = sp.lambdify((x,y),funcion)
 
     while True:
         #creo una copia del punto, para que a la hora de evuluar el x, no lo pierda para y
         _punto_copia = np.copy(_punto)
         #si hay mas iteraicones de las que yo quiero paro
+        #if _iter > _iteraciones or _lam_f(_punto[0],_punto[1]) < np.float64(10**(-14)):
         if _iter > _iteraciones:
+            print ("La funcion ha acabado a las " + str(_iter) + " iteraciones")
             break
         #calculo el gradiente
         _punto[0] = _punto[0]-_lr*_lam_dx(_punto_copia[0],_punto_copia[1])
@@ -49,12 +51,13 @@ def gradiente_descendiente(_lr, _punto, _funcion, _iteraciones):
             #ax.scatter(_punto[0], _punto[1], funcion.evalf(subs={x:_punto[0],y:_punto[1]}), c='r', marker='^')
 
         _iter =_iter+1
-    return _punto   
+    return _punto
 
 #Las varialbes que voy a usar en mi funcion     
 x, y = sp.symbols('x y')
 # creo la funcion
 funcion = (((x**2)*(np.e**y))-(2*(y**2)*(np.e**-x)))**2
+_lam_f = sp.lambdify((x,y),funcion)
 
 #punto en el que inicia
 punto = np.array([1,1],  dtype = float)
@@ -73,7 +76,6 @@ if MOSTRAR_GRAFICAS:
     _X = np.linspace( punto[0]-0.4,punto[0]+0.01,res)
     _Y = np.linspace(punto[1]-0.06,punto[1]+0.01,res)
     _Z = np.zeros((res,res))
-    _lam_f = sp.lambdify((x,y),funcion)
     for ix, xf in enumerate(_X):
         for iy, yf in enumerate(_Y):
             _Z[iy,ix]=_lam_f(xf,yf) # calculo la altura de la funcion de los puntos equiespaciados
@@ -92,14 +94,17 @@ if MOSTRAR_GRAFICAS:
     plt.plot(punto[0],punto[1],".",c="yellow")
 
 #calculo el punto final donde termina el algoritmo
-punto = gradiente_descendiente(lr,punto,funcion,100)
+print ("La altura en la que comienza es: "+str(_lam_f(punto[0],punto[1])))
+punto = gradiente_descendiente(lr,punto,funcion,33)
+print ("El punto en el que acaba es: "+str(punto))
+print ("La altura en la que acaba es: "+str(_lam_f(punto[0],punto[1])))
 
 #muestro donde a acabado el algoritmo
 if MOSTRAR_GRAFICAS:
     
     """
     ax.scatter(punto[0], punto[1], funcion.evalf(subs={x:punto[0],y:punto[1]}), c='w', marker='^')
-    ax.show()   
+    ax.show()
     """
     plt.plot(punto[0],punto[1],".",c="white")
     plt.show()
@@ -107,9 +112,10 @@ if MOSTRAR_GRAFICAS:
 
 #funcion y punto
 funcion = (x**2)+(2*y**2)+(2*sp.sin(2*np.pi*x)*sp.sin(2*np.pi*y))
+_lam_f = sp.lambdify((x,y),funcion)
 punto = np.array([0.1,0.1],  dtype = float)
 #la tasa de aprendizaje
-lr = 0.01
+lr = 0.1
 
 
 #Si quiero mostrar como funciona el gradiente
@@ -121,10 +127,9 @@ if MOSTRAR_GRAFICAS:
     #resolucion con la que quiero mostrar el contorno
     res = 100
     #puntos equiespaciados para calcular la funcion
-    _X = np.linspace( punto[0]-0.5,punto[0]+0.5,res)
-    _Y = np.linspace(punto[1]-0.5,punto[1]+0.5,res)
+    _X = np.linspace( punto[0]-2,punto[0]+2,res)
+    _Y = np.linspace(punto[1]-2,punto[1]+2,res)
     _Z = np.zeros((res,res))
-    _lam_f = sp.lambdify((x,y),funcion)
     for ix, xf in enumerate(_X):
         for iy, yf in enumerate(_Y):
             _Z[iy,ix]=_lam_f(xf,yf) # calculo la altura de la funcion de los puntos equiespaciados
@@ -143,7 +148,10 @@ if MOSTRAR_GRAFICAS:
     plt.plot(punto[0],punto[1],".",c="yellow")
 
 #calculo el punto final donde termina el algoritmo
-punto = gradiente_descendiente(lr,punto,funcion,100)
+print ("La altura en la que comienza es: "+str(_lam_f(punto[0],punto[1])))
+punto = gradiente_descendiente(lr,punto,funcion,49)
+print ("El punto en el que acaba es: "+str(punto))
+print ("La altura en la que acaba es: "+str(_lam_f(punto[0],punto[1])))
 
 #muestro donde a acabado el algoritmo
 if MOSTRAR_GRAFICAS:
