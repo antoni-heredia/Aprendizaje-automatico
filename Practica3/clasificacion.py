fb9f7c28a5e400c1ef20377a3a1f8c7b2cc2c817
Antonio Jesus Heredia <a.heredia.castillo@gmail.com>
2019-05-17 21:59:41 +0200

    Codigo de regresion lineal hecho

---
+++ Practica3/clasificacion.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+import numpy as np
+from sklearn import preprocessing
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import metrics
+
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+def leerArchivo(ruta):
+    f = open(ruta)
+    data = np.genfromtxt(f, delimiter=",")
+    return data[:,:-1],data[:,-1:]
+
+def confusionMatrix(prediccion, y_test, titulo):
+    cm = metrics.confusion_matrix(y_test, prediccion)
+    
+    plt.figure(figsize=(9,9))
+    plt.imshow(cm, interpolation='nearest', cmap='Pastel1')
+    plt.title(titulo, size = 15)
+    plt.colorbar()
+    tick_marks = np.arange(10)
+    plt.xticks(tick_marks, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], rotation=45, size = 10)
+    plt.yticks(tick_marks, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], size = 10)
+    plt.tight_layout()
+    plt.ylabel('Etiquetas reales', size = 15)
+    plt.xlabel('Etiquetas predecidas', size = 15)
+    width, height = cm.shape
+    for x in range(width):
+        for y in range(height):
+            plt.annotate(str(cm[x][y]), xy=(y, x), 
+            horizontalalignment='center',
+            verticalalignment='center')
+    plt.plot()
+
+
+
+X_test,y_test = leerArchivo("datos/digitos/optdigits.tes")
+X_train,y_train = leerArchivo("datos/digitos/optdigits.tra")
+
+
+#----------------Preprocesado de datos
+#Normalizo los datos
+
+#Junto el train y test para poder normalizar los valores de forma correcta
+X = np.concatenate((X_test, X_train))
+
+min_max_scaler = preprocessing.MinMaxScaler()
+
+X = min_max_scaler.fit_transform(X)
+
+#Elimina las columnas que no aportan nada
+selector = VarianceThreshold()
+X = selector.fit_transform(X)
+poly = PolynomialFeatures(degree=2)
+X = poly.fit_transform(X)
+#Una vez normalizados vuelvo a dejar el mismo grupo de train y test
+X_test = X[:X_test.shape[0],:]
+X_train = X[X_test.shape[0]:,:]
+
+#-------------Eleccion de clase de funciones
+
+
+model = linear_model.LogisticRegression( penalty='l2', multi_class='ovr', C=1)
+model.fit(X_train,y_train.ravel())
+print("---------Regresion logistica---------")
+print("E_in: "+str(model.score(X_train,y_train)))
+
+prediccion = model.predict(X_test)
+confusionMatrix(prediccion, y_test, "CM de la Regresion Logistica")
+
+print("E_out: "+str(model.score(X_test,y_test)))
+
+perceptron = linear_model.Perceptron(tol=0, penalty='l1')
+
+print("---------Perceptron---------")
+
+perceptron.fit(X_train,y_train.ravel())
+print("E_in: "+str(perceptron.score(X_train,y_train)))
+print("E_out: "+str(perceptron.score(X_test,y_test)))
---
+++ Practica3/regresion.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 23:59:29 2019
+
+@author: antonio
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+import numpy as np
+from sklearn import preprocessing
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+
+def leerArchivo(ruta):
+    f = open(ruta)
+    data = np.genfromtxt(f, delimiter='\t')
+    return data[:,:-1],data[:,-1:]
+
+
+
+
+
+
+X,y = leerArchivo("datos/air/airfoil_self_noise.dat")
+
+
+#----------------Preprocesado de datos
+#Normalizo los datos
+
+#Junto el train y test para poder normalizar los valores de forma correcta
+
+min_max_scaler = preprocessing.MinMaxScaler()
+
+X = min_max_scaler.fit_transform(X)
+
+#Elimina las columnas que no aportan nada
+selector = VarianceThreshold()
+X = selector.fit_transform(X)
+
+poly = PolynomialFeatures(degree=2)
+X = poly.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+
+modelo = linear_model.LinearRegression()
+modelo.fit(X_train,y_train)
+print("Ein: " + str(modelo.score(X_train,y_train)))
+print("Eout: " + str(modelo.score(X_test,y_test)))
